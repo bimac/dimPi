f04656ec9225282ee2f4199424abe6d775395b18
bimac <bimac@users.noreply.github.com>
2019-07-10 12:58:02 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,26 +6,27 @@ if [[ "$EUID" -ne 0 ]]; then
 	exit 1
 fi
 
-if `systemctl is-active --quiet dimPi.service`; then
-	systemctl stop dimPi.service
-fi
-
+# Stop and disable systemd service
 if `systemctl list-units --full -all | grep -Fq "dimPi.service"`; then
+	systemctl stop dimPi.service
 	systemctl disable dimPi.service
 fi
 
+# Remove systemd service
 TMP="/etc/systemd/system/dimPi.service"
 if test -f "$TMP"; then
 	rm "$TMP"
 	echo "Removed $TMP."
 fi
 
+# Remove dimPi directory
 TMP="/opt/dimPi"
 if test -d "$TMP"; then
 	rm -R "$TMP"
 	echo "Removed $TMP/."
 fi
 
+# Remove symlink to dimPi
 TMP="/usr/local/bin/dimPi"
 if test -h "$TMP"; then
 	unlink "$TMP"
